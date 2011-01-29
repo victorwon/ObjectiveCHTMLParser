4ed6356e0cdec6fc0d681b02a419bed83c5a55d0
Marius Ursache <bamse16@yahoo.com>
2011-01-29 13:09:54 +0200

    Suppress HTML errors

--- HTMLParser.m
+++ HTMLParser.m
@@ -46,7 +46,12 @@
 			CFStringEncoding cfenc = CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding);
 			CFStringRef cfencstr = CFStringConvertEncodingToIANACharSetName(cfenc);
 			const char *enc = CFStringGetCStringPtr(cfencstr, 0);
-			_doc = htmlParseDoc((xmlChar*)[string UTF8String], enc);
+			// _doc = htmlParseDoc((xmlChar*)[string UTF8String], enc);
+			int optionsHtml = 0;
+			optionsHtml = optionsHtml | HTML_PARSE_RECOVER;
+			optionsHtml = optionsHtml | HTML_PARSE_NOERROR; //Uncomment this to see HTML errors
+			optionsHtml = optionsHtml | HTML_PARSE_NOWARNING;
+			_doc = htmlReadDoc ((xmlChar*)[string UTF8String], NULL, enc, optionsHtml);
 		}
 		else 
 		{
